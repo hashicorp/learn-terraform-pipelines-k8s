14e36e457fd802507c856e081cb82c9a2d1849fa
Rachel Sharp <rachel@hashicorp.com>
2021-02-03 15:11:44 -0500

    update for test

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"    
+    organization = "hashicorp-rachel"    
     workspaces {
       name = "firstName-lastInitial-k8s"
     }
