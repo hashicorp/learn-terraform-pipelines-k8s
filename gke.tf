66a7ee6783101b283ccf181124a3388b2539dde8
vodelerk <lallamavioleta@gmail.com>
2021-05-21 12:04:45 -0500

    Remove master auth

--- gke.tf
+++ gke.tf
@@ -9,16 +9,7 @@ resource "google_container_cluster" "engineering" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
-
-  master_auth {
-    username = var.username
-    password = var.password
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-
+  
   ip_allocation_policy {}
 }
 
