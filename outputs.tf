e5b6d23bdbf4ee07bfa65231053f998af1de6ae0
Robin Norwood <rnorwood@hashicorp.com>
2021-05-25 11:29:35 -0500

    Cluster ca cert output is not needed

--- outputs.tf
+++ outputs.tf
@@ -12,11 +12,6 @@ output "access_token" {
   sensitive  = true
 }
 
-output "cluster_ca_certificate" {
-  value     = base64decode(google_container_cluster.engineering.master_auth.0.cluster_ca_certificate)
-  sensitive = true
-}
-
 output "enable_consul_and_vault" {
   value = var.enable_consul_and_vault
 }
