362c81fd0813ce57c73758f5be50eb433ec5d24f
stansec <78606376+stansec@users.noreply.github.com>
2021-06-11 11:00:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     organization = "infrastructure-pipelines-workshop"
     workspaces {
-      name = "john-d-k8s"
+      name = "stanley-a-k8s"
     }
   }
   required_providers {
