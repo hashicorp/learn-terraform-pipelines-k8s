54b367343bc25d71ce74c849042933654b7b937a
Brian McClain <brianmmcclain@gmail.com>
2023-08-16 09:09:51 -0400

    Move node count to a variable

--- gke.tf
+++ gke.tf
@@ -24,7 +24,7 @@ resource "google_container_node_pool" "engineering_preemptible_nodes" {
   location = data.google_compute_zones.available.names.0
 
   version    = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
-  node_count = 3
+  node_count = var.node_count
 
   node_config {
     preemptible  = true
